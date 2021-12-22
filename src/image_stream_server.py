61925e6838eee4d6f984e8f28e36242c8ad36486
smanicome <s.manicome@gmail.com>
2021-12-22 18:37:00 +0100

    Add comments

--- src/image_stream_server.py
+++ src/image_stream_server.py
@@ -10,22 +10,29 @@ from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
 
+# For each frame, convert it into a list of bytes to send to the client with the size of the image
 def send_image(image):
     br = CvBridge()
     try:
+        # wait before being able to write in the socket
+        # we only want to write
         ready_to_read, ready_to_write, in_error = \
             select.select([], [streamConnexion, ], [], 5)
         if ready_to_write:
             try:
+                # first convert the frame to an OpenCV image
                 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
                 result, img_encode = cv2.imencode('.jpg', br.imgmsg_to_cv2(image), encode_param)
+                # retrieve a list of bytes
                 img_data = numpy.array(img_encode)
                 string_data = img_data.tostring()
+                # send both the size of the image and the image itself
                 streamConnexion.send(len(string_data).to_bytes(length=8, byteorder="big"))
                 streamConnexion.send(string_data)
             except CvBridgeError as e:
                 rospy.logerr(e)
     except select.error:
+        # On connection closed, close the connection and terminate the node
         print('Connexion closed, terminating node')
         streamConnexion.close()
         subscriber.unregister()
@@ -33,15 +40,18 @@ def send_image(image):
 
 
 if __name__ == '__main__':
-    rospy.init_node("mobile_controller", anonymous=True)
+    rospy.init_node("image_stream_server", anonymous=True)
     localIpAddress = ip = ni.ifaddresses('enp0s3')[ni.AF_INET][0]['addr']
-    print(localIpAddress)
+
+    # open server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as streamSocket:
         streamSocket.bind((localIpAddress, 3698))
 
         streamSocket.listen(True)
+        # wait for a client to connect
         streamConnexion, mobileAddress = streamSocket.accept()
 
+        # when a client is connected, register camera subscriber and spin
         with streamConnexion:
             print('3698: Connected by', mobileAddress)
             subscriber = rospy.Subscriber("/camera/rgb/image_raw", Image, send_image)
--- src/input_control_server.py
+++ src/input_control_server.py
@@ -4,35 +4,40 @@ import socket
 import struct
 import sys
 
-import numpy
-import rospy
-import cv2
 import netifaces as ni
-from cv_bridge import CvBridge, CvBridgeError
+import rospy
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Image
 
 inputControlConnexion: socket
 
 
+# read inputs from client and translates to Twist messages
 def handle_inputs():
     vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=5)
+    # store data from client
     data = b""
     while True:
         try:
+            # wait before being able to read in the socket
+            # we only want to read
             ready_to_read, ready_to_write, in_error = \
                 select.select([inputControlConnexion, ], [], [], 5)
             if ready_to_read:
                 data += inputControlConnexion.recv(1024)
+                # get speed
                 x = struct.unpack('f', data[:4])[0]
+                # get angle of rotation
                 z = struct.unpack('f', data[4:8])[0]
+                # remove used data
                 data = data[8:]
 
+                # move forward or backward based on rotation, and convert to Twist format
                 if 90 <= z <= 270:
                     speed = -x / 5
                 else:
                     speed = x / 5
 
+                # move left or right based on rotation,  and convert to Twist format
                 if 0 <= z <= 180:
                     angle = -z / 360
                 else:
@@ -40,27 +45,32 @@ def handle_inputs():
 
                 print("Speed: " + str(speed) + " Angle: " + str(angle))
 
+                # send Twist message to move the robot accordingly
                 msg = Twist()
                 msg.linear.x = speed
                 msg.angular.z = angle
                 vel_pub.publish(msg)
         except (select.error, struct.error):
+            # On connection closed, close the connection and terminate the node
             print('Connexion closed, terminating node')
             inputControlConnexion.close()
             sys.exit()
 
 
 if __name__ == '__main__':
-    rospy.init_node("mobile_controller", anonymous=True)
+    rospy.init_node("input_control_server", anonymous=True)
     localIpAddress = ip = ni.ifaddresses('enp0s3')[ni.AF_INET][0]['addr']
-    print(localIpAddress)
+
+    # open server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as streamSocket:
         streamSocket.bind((localIpAddress, 3699))
         streamSocket.listen(True)
+
+        # wait for a client to connect
         inputControlConnexion, mobileAddress = streamSocket.accept()
 
+        # when a client is connected, read for inputs
         with inputControlConnexion:
             print('3699: Connected by', mobileAddress)
             inputControlConnexion.settimeout(None)
             handle_inputs()
-            rospy.spin()
